5eb0753aafb008c818d9c1c5bfe6300d3d5f578d
DirkyJerky <dirkyjerky@me.com>
2014-04-28 10:08:57 -0500

    Start sermester project template

---
+++ src/Semester Proj/FiveHandDraw.cpp
@@ -0,0 +1,33 @@
+// Author: Geoff Yoerger
+// Date: Apr 28, 2014
+// Assignment: Final Project: FiveHandDraw
+// Purpose: Play Five card draw!
+
+#include "stdio.h"
+#include <iostream>
+using namespace std;
+
+/**
+ * @return If the program ended succesfully
+ */
+int main() {
+	//TODO: Define and initialize an array to hold the deck of cards.
+	// -- Print it out
+
+	//TODO: Define arrays to hold the palyers and dealers hand
+
+	//TODO: Generate random numbers between 0 and 51 to select cards from the deck.
+	// Put them in the players and dealers hands, Dont reuse cards;
+	// -- Print players hand
+
+	//TODO:Ack the user what card(s) he wants to discard
+	// Replace with new cards
+	// -- Print: player and dealers hands
+
+	//TODO: Determine the winner, And how they won.
+
+	//TODO: Implement Betting (See sheet)
+
+	//TODO: EC: Do the game until the player reaches $0 or decides to quit.
+	return 0;
+}
